46617f1f150e0bc3d8aaf574be06ae159b506a04
Rob Woodward <rob@emailplus.org>
2023-06-12 11:35:16 +0100

    Fix

--- src/getise/__init__.py
+++ src/getise/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, Rob Woodward. All rights reserved.
 #
-# This file is part of BGP Neighbour Get Tool and is released under the
+# This file is part of Get ISE tool and is released under the
 # "BSD 2-Clause License". Please see the LICENSE file that should
 # have been included as part of this distribution.
 #
-__version__ = '1.1.0'
+__version__ = '1.0.0'
--- src/getise/cli.py
+++ src/getise/cli.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
 # Copyright (c) 2023, Rob Woodward. All rights reserved.
 #
 # This file is part of Get ISE tool and is released under the
 # "BSD 2-Clause License". Please see the LICENSE file that should
 # have been included as part of this distribution.
 #
-#!/usr/bin/env python3
+
 
 import json
 import os
